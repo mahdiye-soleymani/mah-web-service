b0cca81c7716239814edde6488ce239f88d49874
m.soleymani <ma.soleymani@fakour.net>
2022-12-10 15:59:32 +0330

    versioning

---
+++ src/main/java/com/clarity/restfulwebservice/filtering/ActorVersioningController.java
@@ -0,0 +1,42 @@
+package com.clarity.restfulwebservice.filtering;
+
+import com.clarity.restfulwebservice.versioning.ActorV1;
+import com.clarity.restfulwebservice.versioning.ActorV2;
+import com.clarity.restfulwebservice.versioning.Name;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+public class ActorVersioningController {
+    @GetMapping("v1/actor")
+    public ActorV1 actorV1() {
+        return new ActorV1("henry cavil");
+    }
+
+    @GetMapping("v2/actor")
+    public ActorV2 actorV2() {
+        return new ActorV2(new Name("henry", " cavil"));
+    }
+
+    @GetMapping(value = "/actor/param", params = "version=1")
+    public ActorV1 actorParamV1() {
+        return new ActorV1("henry cavil");
+    }
+
+    @GetMapping(value = "/actor/param", params = "version=2")
+    public ActorV2 actorParamV2() {
+        return new ActorV2(new Name("henry", " cavil"));
+    }
+
+    @GetMapping(value = "/actor/header", headers = "VERSION=1")
+    public ActorV1 actorHeaderV1() {
+        return new ActorV1("henry cavil");
+    }
+
+    @GetMapping(value = "/actor/header", headers = "VERSION=2")
+    public ActorV2 actorHeaderV2() {
+        return new ActorV2(new Name("henry", " cavil"));
+    }
+
+
+}
---
+++ src/main/java/com/clarity/restfulwebservice/versioning/Name.java
@@ -0,0 +1,13 @@
+package com.clarity.restfulwebservice.versioning;
+
+import lombok.AllArgsConstructor;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+@Data
+@AllArgsConstructor
+@NoArgsConstructor
+public class Name {
+    private String firstName;
+    private String lastName;
+}
