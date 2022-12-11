0b10762ec7d5fa9f7d0fc859394dadc7dc596473
m.soleymani <ma.soleymani@fakour.net>
2022-12-11 10:47:43 +0330

    disable security add jpa annotation in director(model)

--- src/main/java/com/mahdiyeh/restfulwebservice/model/Director.java
+++ src/main/java/com/mahdiyeh/restfulwebservice/model/Director.java
@@ -1,15 +1,23 @@
 package com.mahdiyeh.restfulwebservice.model;
 
+import jakarta.persistence.Entity;
+import jakarta.persistence.GeneratedValue;
+import jakarta.persistence.Id;
 import jakarta.validation.constraints.Past;
 import jakarta.validation.constraints.Size;
 import lombok.AllArgsConstructor;
 import lombok.Data;
+import lombok.NoArgsConstructor;
 
 import java.util.Date;
 
+@Entity
 @AllArgsConstructor
 @Data
+@NoArgsConstructor
 public class Director {
+    @Id
+    @GeneratedValue
     private Integer id;
     @Size(min = 2, max = 30, message = ".:The name must be between 2 and 35 characters:.")
     private String name;
