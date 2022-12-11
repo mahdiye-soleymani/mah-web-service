dfa3564b4f169ffebaad37dc1109a03869c6bc32
m.soleymani <ma.soleymani@fakour.net>
2022-12-11 12:07:48 +0330

    change entity add data.sql for add to h2 add config for h2 database add some comment

--- src/main/java/com/mahdiyeh/restfulwebservice/model/Director.java
+++ src/main/java/com/mahdiyeh/restfulwebservice/model/Director.java
@@ -21,7 +21,7 @@ public class Director {
     private Integer id;
     @Size(min = 2, max = 30, message = ".:The name must be between 2 and 35 characters:.")
     private String name;
-    @Past(message = ".:Choose a date in the past:.")
-    private Date birthDay;
+//    @Past(message = ".:Choose a date in the past:.")
+//    private Date birthDay;
 
 }
---
+++ src/main/resources/data.sql
@@ -0,0 +1,11 @@
+insert into director
+values (13, 'Coppola');
+
+insert into director
+values (14, 'Hitchcock');
+
+insert into director
+values (15, 'Nolan');
+
+-- INSERT INTO director (id, name) VALUES (1, 'USA');
+-- INSERT INTO director (id, name) VALUES (2, 'France');
